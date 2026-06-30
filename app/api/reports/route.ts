14bace308e3b7e43bdc1a75efe225c63fbd96544
Srinivas P <srinivas2006.srini@gmail.com>
2026-06-30 22:58:57 +0530

    DF

--- app/api/reports/route.ts
+++ app/api/reports/route.ts
@@ -19,7 +19,7 @@ export async function GET(request: Request) {
           getAll() {
             return cookieStore.getAll();
           },
-          setAll(cookiesToSet) {
+          setAll(cookiesToSet: any[]) {
             try {
               cookiesToSet.forEach(({ name, value, options }) =>
                 cookieStore.set(name, value, options)
@@ -61,7 +61,7 @@ export async function POST(request: Request) {
           getAll() {
             return cookieStore.getAll();
           },
-          setAll(cookiesToSet) {
+          setAll(cookiesToSet: any[]) {
             try {
               cookiesToSet.forEach(({ name, value, options }) =>
                 cookieStore.set(name, value, options)
